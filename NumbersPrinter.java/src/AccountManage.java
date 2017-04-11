afed194ea84e28aa6d0bf03673d4fc766ab973ae
RUSHITHA12 <rushitha.b@gmail.com>
2017-04-11 18:14:02 +0530

    correction in account management program

--- NumbersPrinter.java/src/AccountManage.java
+++ NumbersPrinter.java/src/AccountManage.java
@@ -6,12 +6,12 @@ public class AccountManage {
 		int amount;
 		account Account=new account();
 		System.out.println("enter number 1 to withdraw and number 2 deposit");
-		Scanner number=new Scanner(System.in);
-		int digit =number.nextInt();
+		Scanner scan=new Scanner(System.in);
+		int digit =scan.nextInt();
 		if(digit==1){		
 		System.out.println("enter the amount to withdraw");
-		Scanner scan =new Scanner(System.in);
-        amount = scan.nextInt();
+		
+                amount = scan.nextInt();
         if(amount<Account.balance){
         int balance;
         balance = Account.withdraw(amount);
@@ -23,8 +23,8 @@ public class AccountManage {
 		}
 		else{
 		System.out.println("enter the amount to deposit");
-		Scanner scan1 =new Scanner(System.in);
-        amount = scan1.nextInt();
+		
+         amount = scan.nextInt();
         int balance;
         balance=Account.credit(amount);
         System.out.println("available new balance is " + balance);
